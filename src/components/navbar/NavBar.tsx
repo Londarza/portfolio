19af9b0042c22af9e0baea4ff981db1cd80135c3
Londarza <lautaro.ondarza@gmail.com>
2025-03-05 20:21:23 -0300

    se dan nuevos estilos a navbar

--- src/components/navbar/NavBar.tsx
+++ src/components/navbar/NavBar.tsx
@@ -3,31 +3,54 @@
 import images from "@/constants/constants";
 import Image from "next/image";
 import '@/components/navbar/NavBar.scss'
+import { HiMenuAlt4, HiX } from 'react-icons/hi'
+import { motion } from 'framer-motion'
 
 //import Link from "next/link";
 //import Image from "next/image";
 //import { FaBars } from "react-icons/fa";
-//import { useState } from "react";
+import { useState } from "react";
 
 
 const Navbar: React.FC = () => {
   //const [isMenuOpen, setIsMenuOpen] = useState(false);
-  
+  const [toggle, setToggle] = useState(false)
+
   return (
     <div className="app">
-    <nav className="app__navbar">
-      <div className="app__navbar-logo">
-        <Image src={images.logo} alt="logo" />
-      </div>
-      <ul className="app__navbar-links">
-        {['Home', 'About','Contact', 'Work', 'Skills'].map((item)=>(
-          <li className="app__flex p-text" key={`Link-${item}`}>
-            <div></div>
-            <a href={`/${item}`}> {item}</a>
-          </li>
-        ))}
-      </ul>
-    </nav>
+      <nav className="app__navbar">
+        <div className="app__navbar-logo">
+          <Image src={images.logo} alt="logo" />
+        </div>
+        <ul className="app__navbar-links">
+          {['Home', 'About', 'Contact', 'Work', 'Skills'].map((item) => (
+            <li className="app__flex p-text" key={`Link-${item}`}>
+              <div></div>
+              <a href={`/${item}`}> {item}</a>
+            </li>
+          ))}
+        </ul>
+        <div className='app__navbar-menu'>
+          <HiMenuAlt4 onClick={() => setToggle(true)} />
+
+          {toggle && (
+            <motion.div
+              whileInView={{ x: [300, 0] }}
+              transition={{ duration: 0.85, ease: 'easeOut' }}
+            >
+              <HiX onClick={() => setToggle(false)} />
+
+              {['Home', 'About', 'Contact', 'Work', 'Skills'].map((item) => (
+                <li className="" key={item}>                  
+                  <a onClick={() => setToggle(false)} href={`/${item}`}> {item}</a>
+                </li>
+              ))}
+
+            </motion.div>
+          )}
+        </div>
+
+      </nav>
 
     </div>
 
