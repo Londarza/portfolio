016aa4bd5f91d52029d13b055dbdea7bfd3bd26c
Londarza <lautaro.ondarza@gmail.com>
2025-03-25 00:19:42 -0300

    se finaliza con skills section, se consume correctamente de sanity, se instala tooltip, se dan estilos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@
 import About  from "@/containers/About/About";
 import Header from "../containers/Header/Header";
 import  Work  from "@/containers/work/Work";
+import  Skills  from "@/containers/skills/Skills";
 //import LandingView from "@/views/landing/LandingView";
 
 export default function Home() {
@@ -11,6 +12,7 @@ export default function Home() {
       <Header />
       <About/>
       <Work/>
+      <Skills/>
     </div>
   );
 }
--- src/containers/skills/Skills.tsx
+++ src/containers/skills/Skills.tsx
@@ -1,8 +1,120 @@
-import React from 'react'
+"use client"
+import React, { useEffect, useState } from 'react'
 //styles
 import './Skills.scss'
-export const Skills = () => {
+import {Tooltip} from 'react-tooltip'
+import { motion } from 'framer-motion'
+import AppWrap from '@/wrapper/AppWrap'
+import { urlFor, client } from '@/client'
+import Image from 'next/image'
+
+
+const Skills = () => {
+  const [experiences, setexperiences] = useState([])
+  const [skills, setSkills] = useState([])
+
+
+  useEffect(() => {
+    const expQuery = '*[_type == "experiences"]{year, works[]{name, company, desc}}';
+
+    const skillsQuery = '*[_type == "skills"]{name, bgColor, icon{asset->{_id, url}}}';
+
+    client.fetch(expQuery)
+      .then((data) => {
+        console.log('experiences',data);
+        
+        setexperiences(data)
+      })
+    client.fetch(skillsQuery)
+      .then((data) => {
+        setSkills(data)
+      })
+
+      
+  }, [])
+
   return (
-    <div>Skills</div>
+    <>
+      <h2 className='head-text'> Skills y Experiencias</h2>
+
+
+      {/* skills seccion*/}
+      <div className='app__skills-container'>
+        <motion.div className='app__skills-list' >
+          {skills?.map((skill, index) => (
+            <motion.div
+              key={index}
+              whileInView={{ opacity: [0, 1] }}
+              transition={{ duration: 0.5 }}
+              className='app__skills-item app__flex'
+            >
+
+              <div className='app__flex' style={{ backgroundColor: skill.bgColor }}>
+                <Image
+                  className='app__skills-image'
+                  src={urlFor(skill.icon).url()}
+                  alt={skill.name}
+                  width={300}
+                  height={200}
+                />
+
+              </div>
+              <p className='p-text'> {skill.name}</p>
+            </motion.div>
+
+
+
+          ))
+
+
+
+          }
+        </motion.div>
+
+
+        {/* experience seccion*/}
+
+        <div className="app__skills-exp">
+          {experiences?.map((experience,index) => (
+            <motion.div
+              className="app__skills-exp-item"
+              key={index+experience.year}
+            >
+              <div className="app__skills-exp-year">
+                <p className="bold-text">{experience.year}</p>
+              </div>
+              <motion.div className="app__skills-exp-works">
+                {experience.works.map((work,index) => (
+                  <React.Fragment key={index + work.name}>
+                    <motion.div
+                      whileInView={{ opacity: [0, 1] }}
+                      transition={{ duration: 0.5 }}
+                      className="app__skills-exp-work"
+                      data-tooltip-id={work.name}
+                      key={index}
+                    >
+                      <h4 className="bold-text">{work.name}</h4>
+                      <p className="p-text">{work.company}</p>
+                    </motion.div>
+                    <Tooltip
+                      id={work.name}
+                      style={{ backgroundColor: '#000', color: '#fff' }}
+                      className="skills-tooltip"
+                    >
+                      {work.desc}
+                    </Tooltip>
+                  </React.Fragment>
+                ))}
+              </motion.div>
+            </motion.div>
+          ))}
+        </div>
+
+        
+      </div>
+    </>
   )
 }
+
+
+export  default AppWrap(Skills, 'Skills')
