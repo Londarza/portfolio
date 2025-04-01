1f643dbcbd9c61cad4a4f05fb6308dcf929ded0e
Londarza <lautaro.ondarza@gmail.com>
2025-04-01 15:21:40 -0300

    se cambian tipados de wrappers para usar REACT.FC

--- src/wrapper/AppWrap.tsx
+++ src/wrapper/AppWrap.tsx
@@ -1,25 +1,26 @@
 import NavigationDots from '@/components/NavigationDots'
 import SocialMedia from '@/components/SocialMedia'
 import React from 'react'
-const AppWrap = (Component: React.ComponentType, idName : string, classNames : string) => function HOC(): JSX.Element {
-  return (
-  <div 
-    id={idName}
-    className={`app__container ${classNames}`}
-   >
-    <SocialMedia/>
 
-    <div className='app__wrapper app__flex'>
-      <Component/>
-      <div className='copyright'>
-        <p className='p-text'>2025 Lautaro Ondarza </p>
-        <p className='p-text'>Todos los derechos reservados.</p>
 
+const AppWrap = (Component: React.ComponentType, idName: string, classNames: string) => {
+  const HOC: React.FC = () => {
+    return (
+      <div id={idName} className={`app__container ${classNames}`}>
+        <SocialMedia />
+        <div className='app__wrapper app__flex'>
+          <Component />
+          <div className='copyright'>
+            <p className='p-text'>2025 Lautaro Ondarza </p>
+            <p className='p-text'>Todos los derechos reservados.</p>
+          </div>
+        </div>
+        <NavigationDots active={idName} />
       </div>
-    </div>
-    <NavigationDots active={idName}/>
-  </div>
-  )
-}
+    );
+  };
 
-export default AppWrap
+  return HOC;
+};
+
+export default AppWrap;
--- src/wrapper/MotionWrap.tsx
+++ src/wrapper/MotionWrap.tsx
@@ -1,15 +1,20 @@
 import React from 'react'
 import {motion} from 'framer-motion'
-const MotionWrap = (Component:React.FC, classNames:string) => function HOC(): JSX.Element {
-  return (
-    <motion.div
-    whileInView={{opacity:[0,0,1], y:[100,50,0]}}
-    transition={{duration:0.6}}
-    className={`${classNames} app__flex`}>
-        <Component/>
-    </motion.div>
-  )
-}
 
+const MotionWrap = (Component: React.ComponentType, classNames: string) => {
+  const HOC: React.FC = () => {
+    return (
+      <motion.div
+        whileInView={{ opacity: [0, 0, 1], y: [100, 50, 0] }}
+        transition={{ duration: 0.6 }}
+        className={`${classNames} app__flex`}
+      >
+        <Component />
+      </motion.div>
+    );
+  };
 
-export default MotionWrap
+  return HOC;
+};
+
+export default MotionWrap;
